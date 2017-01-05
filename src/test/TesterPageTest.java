7cda52174be214e7db6d685e70886b4a33b740f6
Anastasiia Kolchanova <anastasiia.kolchanova@gmail.com>
2017-01-05 15:53:40 +0200

    added files for test

---
+++ src/test/TesterPageTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class TesterPageTest {
+    public static WebDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "chromedriver/chromedriver.exe");
+    }
+
+    //@After
+    //public void tearDown(){
+    //    driver.close();
+    //}
+
+    @Test
+    public void googleTest() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.get("http://sta-kiv-gt1-setup01-spp-01.nix.cydmodule.com:8080/admin/tester.jsp");
+        Thread.sleep(2000);
+        WebElement login = driver.findElement(By.cssSelector("input[name=login]"));
+        login.sendKeys("manager");
+        Thread.sleep(2000);
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("manager");
+        Thread.sleep(2000);
+        WebElement goToTester = driver.findElement(By.cssSelector("input[value=Login]"));
+        goToTester.click();
+
+        //find.click();
+        //Thread.sleep(2000);
+    }
+}
